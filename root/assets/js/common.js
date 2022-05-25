3097a0bc134bcc579c81b80d4a7c0508ab8cbbf6
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2022-05-25 17:03:38 +0700

    tungnt

--- root/assets/js/common.js
+++ root/assets/js/common.js
@@ -1,32 +1,15 @@
-// /** First we get all the non-loaded image elements **/
-// var lazyImages = [].slice.call(document.querySelectorAll(".lazy"));
-// /** Then we set up a intersection observer watching over those images and whenever any of those becomes visible on the view then replace the placeholder image with actual one, remove the non-loaded class and then unobserve for that element **/
-// let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-//     entries.forEach(function(entry) {
-//         if (entry.isIntersecting) {
-//             let lazyImage = entry.target;
-//             lazyImage.src = lazyImage.dataset.src;
-//             lazyImage.classList.remove("lazy");
-//             lazyImageObserver.unobserve(lazyImage);
-//         }
-//     });
-// });
-// /** Now observe all the non-loaded images using the observer we have setup above **/
-// lazyImages.forEach(function(lazyImage) {
-//     lazyImageObserver.observe(lazyImage);
-// });
 if ('loading' in HTMLImageElement.prototype) {
-    const images = document.querySelectorAll('img[loading="lazy"]');
-    images.forEach(img => {
-      img.src = img.dataset.src;
-    });
-  } else {
-    // Dynamically import the LazySizes library
-    const script = document.createElement('script');
-    script.src =
-      'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js';
-    document.body.appendChild(script);
-  };
+  const images = document.querySelectorAll('img[loading="lazy"]');
+  images.forEach(img => {
+    img.src = img.dataset.src;
+  });
+} else {
+  // Dynamically import the LazySizes library
+  const script = document.createElement('script');
+  script.src =
+    'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js';
+  document.body.appendChild(script);
+};
 
 function toggleMenu(){
     var menu = document.getElementsByClassName("nav-menu");
