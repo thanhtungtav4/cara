f54e4a4cd4410568fbc044d5a2c7e413c2987e3d
tungnt <nguyenthanhtung74119@gmail.com>
2022-06-02 22:36:19 +0700

    tungnt

--- root/assets/js/common.js
+++ root/assets/js/common.js
@@ -48,12 +48,25 @@ function toggleMenu(){
     }
 
 }
-// function toggleCart(){
-//   if (window.screen.width >= 768) {
-//       var cartToggle = document.getElementsByClassName("m-cart");
-//       cartToggle[0].classList.toggle("js-active");
-//   }
-//   else{
-//       window.location.href = '/gio-hang';
-//   }
-// }
+
+jQuery.event.special.touchstart = {
+  setup: function( _, ns, handle ) {
+      this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+  }
+};
+jQuery.event.special.touchmove = {
+  setup: function( _, ns, handle ) {
+      this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+  }
+};
+jQuery.event.special.wheel = {
+  setup: function( _, ns, handle ){
+      this.addEventListener("wheel", handle, { passive: true });
+  }
+};
+jQuery.event.special.mousewheel = {
+  setup: function( _, ns, handle ){
+      this.addEventListener("mousewheel", handle, { passive: true });
+  }
+};
+
